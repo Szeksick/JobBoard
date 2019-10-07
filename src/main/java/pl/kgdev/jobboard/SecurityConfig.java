c71834fda9b0e4a1dd6e7cf26bd1f3d5d624b7fd
Szeksick <konradgugala@gmail.com>
2019-10-07 10:00:33 +0200

    simple controller with index.html tmeplate

--- src/main/java/pl/kgdev/jobboard/SecurityConfig.java
+++ src/main/java/pl/kgdev/jobboard/SecurityConfig.java
@@ -1,4 +1,15 @@
 package pl.kgdev.jobboard;
 
-public class SecurityConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/").permitAll();
+    }
 }
