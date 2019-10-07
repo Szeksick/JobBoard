623f0f57acabada23044d085c6dbaf7081bef4e5
Szeksick <konradgugala@gmail.com>
2019-10-07 09:40:14 +0200

    Initial commit

---
+++ src/main/java/pl/kgdev/jobboard/JobboardApplication.java
@@ -0,0 +1,13 @@
+package pl.kgdev.jobboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobboardApplication.class, args);
+    }
+
+}
