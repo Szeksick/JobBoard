54f6736ce672241bb6c02d22d00ac34de3175c12
Szeksick <konradgugala@gmail.com>
2019-12-07 12:12:27 +0100

    Entities Added

--- src/main/java/pl/kgdev/jobboard/controllers/HomeController.java
+++ src/main/java/pl/kgdev/jobboard/controllers/HomeController.java
@@ -1,11 +1,13 @@
 package pl.kgdev.jobboard.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.kgdev.jobboard.entities.JobOffer;
 import pl.kgdev.jobboard.repositories.CategoryRepository;
